20b17c760e31d3a66109fc0c063172b37dda2008
Dunkhan <drunken4ever@gmail.com>
2019-03-15 14:48:42 +0100

    Changed tabs to spaces

--- Source/CombatExtended/CombatExtended/Gizmos/Command_Reload.cs
+++ Source/CombatExtended/CombatExtended/Gizmos/Command_Reload.cs
@@ -10,109 +10,109 @@ using CombatExtended.CombatExtended.Things;
 
 namespace CombatExtended
 {
-	public class Command_Reload : Command_Action
-	{
-		public CompAmmoUser compAmmo;
+    public class Command_Reload : Command_Action
+    {
+        public CompAmmoUser compAmmo;
 
-		public override void ProcessInput(Event ev)
-		{
-			if (compAmmo == null)
-			{
-				Log.Error("Command_Reload without ammo comp");
-				return;
-			}
-			if (compAmmo.UseAmmo
-				&& (compAmmo.CompInventory != null || compAmmo.turret != null)
-				|| action == null)
-			{
-				Find.WindowStack.Add(MakeAmmoMenu());
-			}
-			else if (compAmmo.SelectedAmmo != compAmmo.CurrentAmmo || compAmmo.CurMagCount < compAmmo.Props.magazineSize)
-			{
-				base.ProcessInput(ev);
-			}
-			// Show we learned something by clicking this
-			if (!tutorTag.NullOrEmpty()) PlayerKnowledgeDatabase.KnowledgeDemonstrated(ConceptDef.Named(tutorTag), KnowledgeAmount.Total);
-		}
+        public override void ProcessInput(Event ev)
+        {
+            if (compAmmo == null)
+            {
+                Log.Error("Command_Reload without ammo comp");
+                return;
+            }
+            if (compAmmo.UseAmmo
+                && (compAmmo.CompInventory != null || compAmmo.turret != null)
+                || action == null)
+            {
+                Find.WindowStack.Add(MakeAmmoMenu());
+            }
+            else if (compAmmo.SelectedAmmo != compAmmo.CurrentAmmo || compAmmo.CurMagCount < compAmmo.Props.magazineSize)
+            {
+                base.ProcessInput(ev);
+            }
+            // Show we learned something by clicking this
+            if (!tutorTag.NullOrEmpty()) PlayerKnowledgeDatabase.KnowledgeDemonstrated(ConceptDef.Named(tutorTag), KnowledgeAmount.Total);
+        }
 
-		private FloatMenu MakeAmmoMenu()
-		{
-			List<ThingDef> ammoList = new List<ThingDef>();      // List of all ammo types the gun can use and the pawn has in his inventory
-			if (compAmmo.turret != null)
-			{
-				// If we have no inventory available (e.g. manned turret), add all possible ammo types to the selection
-				foreach (AmmoLink link in compAmmo.Props.ammoSet.ammoTypes)
-				{
-					ammoList.Add(link.ammo);
-				}
-			}
-			else
-			{
-				// Iterate through all suitable ammo types and check if they're in our inventory
-				foreach (AmmoLink curLink in compAmmo.Props.ammoSet.ammoTypes)
-				{
-					if (compAmmo.CompInventory.ammoList.Any(x => x.def == curLink.ammo))
-						ammoList.Add(curLink.ammo);
-				}
-			}
+        private FloatMenu MakeAmmoMenu()
+        {
+            List<ThingDef> ammoList = new List<ThingDef>();      // List of all ammo types the gun can use and the pawn has in his inventory
+            if (compAmmo.turret != null)
+            {
+                // If we have no inventory available (e.g. manned turret), add all possible ammo types to the selection
+                foreach (AmmoLink link in compAmmo.Props.ammoSet.ammoTypes)
+                {
+                    ammoList.Add(link.ammo);
+                }
+            }
+            else
+            {
+                // Iterate through all suitable ammo types and check if they're in our inventory
+                foreach (AmmoLink curLink in compAmmo.Props.ammoSet.ammoTypes)
+                {
+                    if (compAmmo.CompInventory.ammoList.Any(x => x.def == curLink.ammo))
+                        ammoList.Add(curLink.ammo);
+                }
+            }
 
-			// Append float menu options for every available ammo type
-			List<FloatMenuOption> floatOptionList = new List<FloatMenuOption>();
-			if (ammoList.NullOrEmpty())
-			{
-				floatOptionList.Add(new FloatMenuOption("CE_OutOfAmmo".Translate(), null));
-			}
-			else
-			{
-				// Append all available ammo types
-				foreach (ThingDef curDef in ammoList)
-				{
-					AmmoDef ammoDef = (AmmoDef)curDef;
-					floatOptionList.Add(new FloatMenuOption(ammoDef.ammoClass.LabelCap, new Action(delegate
-					{
-						bool shouldReload = Controller.settings.AutoReloadOnChangeAmmo && (compAmmo.SelectedAmmo != ammoDef || compAmmo.CurMagCount < compAmmo.Props.magazineSize) && compAmmo.turret?.MannableComp == null;
-						compAmmo.SelectedAmmo = ammoDef;
-						if (shouldReload)
-						{
-							this.TryStartSyncReload();
-						}
-					})));
-				}
-			}
-			// Append unload command
-			var hasOperator = compAmmo.Wielder != null || (compAmmo.turret?.MannableComp?.MannedNow ?? false);
-			if (compAmmo.UseAmmo && hasOperator && compAmmo.HasMagazine && compAmmo.CurMagCount > 0)
-			{
-				floatOptionList.Add(new FloatMenuOption("CE_UnloadLabel".Translate(), new Action(delegate { compAmmo.TryUnload(); })));
-			}
-			// Append reload command
-			if (compAmmo.HasMagazine && hasOperator)
-			{
-				floatOptionList.Add(new FloatMenuOption("CE_ReloadLabel".Translate(), new Action(this.TryStartSyncReload)));
-			}
-			return new FloatMenu(floatOptionList);
-		}
+            // Append float menu options for every available ammo type
+            List<FloatMenuOption> floatOptionList = new List<FloatMenuOption>();
+            if (ammoList.NullOrEmpty())
+            {
+                floatOptionList.Add(new FloatMenuOption("CE_OutOfAmmo".Translate(), null));
+            }
+            else
+            {
+                // Append all available ammo types
+                foreach (ThingDef curDef in ammoList)
+                {
+                    AmmoDef ammoDef = (AmmoDef)curDef;
+                    floatOptionList.Add(new FloatMenuOption(ammoDef.ammoClass.LabelCap, new Action(delegate
+                    {
+                        bool shouldReload = Controller.settings.AutoReloadOnChangeAmmo && (compAmmo.SelectedAmmo != ammoDef || compAmmo.CurMagCount < compAmmo.Props.magazineSize) && compAmmo.turret?.MannableComp == null;
+                        compAmmo.SelectedAmmo = ammoDef;
+                        if (shouldReload)
+                        {
+                            this.TryStartSyncReload();
+                        }
+                    })));
+                }
+            }
+            // Append unload command
+            var hasOperator = compAmmo.Wielder != null || (compAmmo.turret?.MannableComp?.MannedNow ?? false);
+            if (compAmmo.UseAmmo && hasOperator && compAmmo.HasMagazine && compAmmo.CurMagCount > 0)
+            {
+                floatOptionList.Add(new FloatMenuOption("CE_UnloadLabel".Translate(), new Action(delegate { compAmmo.TryUnload(); })));
+            }
+            // Append reload command
+            if (compAmmo.HasMagazine && hasOperator)
+            {
+                floatOptionList.Add(new FloatMenuOption("CE_ReloadLabel".Translate(), new Action(this.TryStartSyncReload)));
+            }
+            return new FloatMenu(floatOptionList);
+        }
 
-		public void TryStartSyncReload()
-		{
-			Log.Message("Checking multiplayer");
-			if (MultiplayerSynchroniser.IsActive)
-			{
-				Log.Message("Attempting to sync reload");
-				MultiplayerSynchroniser.SyncTryStartReload(this.compAmmo.Holder?.ThingID ?? this.compAmmo.turret.ThingID, this.compAmmo.SelectedAmmo.defName);
-			}
-			else
-			{
-				Log.Message("Multiplayer not active");
-				if (compAmmo.turret != null)
-				{
-					compAmmo.turret.TryOrderReload();
-				}
-				else
-				{
-					compAmmo.TryStartReload();
-				}
-			}
-		}
-	}
+        public void TryStartSyncReload()
+        {
+            Log.Message("Checking multiplayer");
+            if (MultiplayerSynchroniser.IsActive)
+            {
+                Log.Message("Attempting to sync reload");
+                MultiplayerSynchroniser.SyncTryStartReload(this.compAmmo.Holder?.ThingID ?? this.compAmmo.turret.ThingID, this.compAmmo.SelectedAmmo.defName);
+            }
+            else
+            {
+                Log.Message("Multiplayer not active");
+                if (compAmmo.turret != null)
+                {
+                    compAmmo.turret.TryOrderReload();
+                }
+                else
+                {
+                    compAmmo.TryStartReload();
+                }
+            }
+        }
+    }
 }
--- Source/CombatExtended/Harmony/HarmonyBase.cs
+++ Source/CombatExtended/Harmony/HarmonyBase.cs
@@ -25,97 +25,97 @@ using CombatExtended.CombatExtended.Things;
 
 namespace CombatExtended.Harmony
 {
-	public static class HarmonyBase
-	{
-		private static HarmonyInstance harmony = null;
+    public static class HarmonyBase
+    {
+        private static HarmonyInstance harmony = null;
 
-		/// <summary>
-		/// Fetch CombatExtended's instance of Harmony.
-		/// </summary>
-		/// <remarks>One should only have a single instance of Harmony per Assembly.</remarks>
-		static internal HarmonyInstance instance
-		{
-			get
-			{
-				if (harmony == null)
-					harmony = harmony = HarmonyInstance.Create("CombatExtended.Harmony");
-				return harmony;
-			}
-		}
+        /// <summary>
+        /// Fetch CombatExtended's instance of Harmony.
+        /// </summary>
+        /// <remarks>One should only have a single instance of Harmony per Assembly.</remarks>
+        static internal HarmonyInstance instance
+        {
+            get
+            {
+                if (harmony == null)
+                    harmony = harmony = HarmonyInstance.Create("CombatExtended.Harmony");
+                return harmony;
+            }
+        }
 
-		public static void InitPatches()
-		{
-			// Remove the remark on the following to debug all auto patches.
-			//HarmonyInstance.DEBUG = true;
-			instance.PatchAll(Assembly.GetExecutingAssembly());
-			// Keep the following remarked to also debug manual patches.
-			//HarmonyInstance.DEBUG = false;
+        public static void InitPatches()
+        {
+            // Remove the remark on the following to debug all auto patches.
+            //HarmonyInstance.DEBUG = true;
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+            // Keep the following remarked to also debug manual patches.
+            //HarmonyInstance.DEBUG = false;
 
-			// Manual patches
-			PatchThingOwner();
-			PatchHediffWithComps();
-			MultiplayerCompatibility();
-			Harmony_GenRadial_RadialPatternCount.Patch();
-			PawnColumnWorkers_Resize.Patch();
-			PawnColumnWorkers_SwapButtons.Patch();
-		}
+            // Manual patches
+            PatchThingOwner();
+            PatchHediffWithComps();
+            MultiplayerCompatibility();
+            Harmony_GenRadial_RadialPatternCount.Patch();
+            PawnColumnWorkers_Resize.Patch();
+            PawnColumnWorkers_SwapButtons.Patch();
+        }
 
-		#region Patch helper methods
+        #region Patch helper methods
 
-		private static void PatchThingOwner()
-		{
-			// Need to patch ThingOwner<T> manually for all child classes of Thing
-			var postfixTryAdd = typeof(Harmony_ThingOwner_TryAdd_Patch).GetMethod("Postfix");
-			var postfixTake = typeof(Harmony_ThingOwner_Take_Patch).GetMethod("Postfix");
-			var postfixRemove = typeof(Harmony_ThingOwner_Remove_Patch).GetMethod("Postfix");
+        private static void PatchThingOwner()
+        {
+            // Need to patch ThingOwner<T> manually for all child classes of Thing
+            var postfixTryAdd = typeof(Harmony_ThingOwner_TryAdd_Patch).GetMethod("Postfix");
+            var postfixTake = typeof(Harmony_ThingOwner_Take_Patch).GetMethod("Postfix");
+            var postfixRemove = typeof(Harmony_ThingOwner_Remove_Patch).GetMethod("Postfix");
 
-			var baseType = typeof(Thing);
-			var types = baseType.AllSubclassesNonAbstract().Add(baseType);
-			foreach (Type current in types)
-			{
-				var type = typeof(ThingOwner<>).MakeGenericType(current);
-				instance.Patch(type.GetMethod("TryAdd", new Type[] { typeof(Thing), typeof(bool) }), null, new HarmonyMethod(postfixTryAdd));
-				instance.Patch(type.GetMethod("Take", new Type[] { typeof(Thing), typeof(int) }), null, new HarmonyMethod(postfixTake));
-				instance.Patch(type.GetMethod("Remove", new Type[] { typeof(Thing) }), null, new HarmonyMethod(postfixRemove));
-			}
-		}
+            var baseType = typeof(Thing);
+            var types = baseType.AllSubclassesNonAbstract().Add(baseType);
+            foreach (Type current in types)
+            {
+                var type = typeof(ThingOwner<>).MakeGenericType(current);
+                instance.Patch(type.GetMethod("TryAdd", new Type[] { typeof(Thing), typeof(bool) }), null, new HarmonyMethod(postfixTryAdd));
+                instance.Patch(type.GetMethod("Take", new Type[] { typeof(Thing), typeof(int) }), null, new HarmonyMethod(postfixTake));
+                instance.Patch(type.GetMethod("Remove", new Type[] { typeof(Thing) }), null, new HarmonyMethod(postfixRemove));
+            }
+        }
 
-		private static void PatchHediffWithComps()
-		{
-			var postfixBleedRate = typeof(Harmony_HediffWithComps_BleedRate_Patch).GetMethod("Postfix");
-			var baseType = typeof(HediffWithComps);
-			var types = baseType.AllSubclassesNonAbstract().Add(baseType);
-			foreach (Type cur in types)
-			{
-				instance.Patch(cur.GetProperty("BleedRate").GetGetMethod(), null, new HarmonyMethod(postfixBleedRate));
-			}
-		}
+        private static void PatchHediffWithComps()
+        {
+            var postfixBleedRate = typeof(Harmony_HediffWithComps_BleedRate_Patch).GetMethod("Postfix");
+            var baseType = typeof(HediffWithComps);
+            var types = baseType.AllSubclassesNonAbstract().Add(baseType);
+            foreach (Type cur in types)
+            {
+                instance.Patch(cur.GetProperty("BleedRate").GetGetMethod(), null, new HarmonyMethod(postfixBleedRate));
+            }
+        }
 
-		private static void MultiplayerCompatibility()
-		{
-			var Multiplayer = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(assembly => assembly.GetName().Name == "Multiplayer");
-			if (Multiplayer == null)
-			{
-				return;
-			}
-			MultiplayerSynchroniser.IsActive = true;
-			MethodInfo original  = AccessTools.Method("Multiplayer.Client.SyncHandlers:Init");
-			instance.Patch(original, new HarmonyMethod(SymbolExtensions.GetMethodInfo(() => PatchMultiPlayerSyncMethod())));
-			
-		}
+        private static void MultiplayerCompatibility()
+        {
+            var Multiplayer = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(assembly => assembly.GetName().Name == "Multiplayer");
+            if (Multiplayer == null)
+            {
+                return;
+            }
+            MultiplayerSynchroniser.IsActive = true;
+            MethodInfo original = AccessTools.Method("Multiplayer.Client.SyncHandlers:Init");
+            instance.Patch(original, new HarmonyMethod(SymbolExtensions.GetMethodInfo(() => PatchMultiPlayerSyncMethod())));
 
-		private static void PatchMultiPlayerSyncMethod()
-		{
-			AccessTools.Method("Multiplayer.Client.SyncMethod:Register").Invoke(null, new object[] { typeof(MultiplayerSynchroniser), nameof(MultiplayerSynchroniser.SyncTryStartReload), null });
-		}
+        }
 
-		#endregion
+        private static void PatchMultiPlayerSyncMethod()
+        {
+            AccessTools.Method("Multiplayer.Client.SyncMethod:Register").Invoke(null, new object[] { typeof(MultiplayerSynchroniser), nameof(MultiplayerSynchroniser.SyncTryStartReload), null });
+        }
 
-		#region Utility_Methods
+        #endregion
 
-		// Remarked the following block since time is a factor, played with it yesterday but it will probably eat too much time to finish and is probably a better fit for 
-		// a Harmony PR.
-		/*
+        #region Utility_Methods
+
+        // Remarked the following block since time is a factor, played with it yesterday but it will probably eat too much time to finish and is probably a better fit for 
+        // a Harmony PR.
+        /*
         /// <summary>
         /// Returns a bool indicating if the types are compatible (castable).  Optional bool does implicit specific check.  That is that one can cast from into to.
         /// </summary>
@@ -221,116 +221,116 @@ namespace CombatExtended.Harmony
         }
         */
 
-		internal static LocalBuilder[] GetLocals(ILGenerator il)
-		{
-			return Traverse.Create(il).Field("locals").GetValue<LocalBuilder[]>();
-		}
+        internal static LocalBuilder[] GetLocals(ILGenerator il)
+        {
+            return Traverse.Create(il).Field("locals").GetValue<LocalBuilder[]>();
+        }
 
-		/// <summary>
-		/// branchOps is used by isBranch utility method.
-		/// </summary>
-		private static readonly OpCode[] branchOps = {
-			OpCodes.Br, OpCodes.Br_S, OpCodes.Brfalse, OpCodes.Brfalse_S, OpCodes.Brtrue, OpCodes.Brtrue_S, // basic branches
+        /// <summary>
+        /// branchOps is used by isBranch utility method.
+        /// </summary>
+        private static readonly OpCode[] branchOps = {
+            OpCodes.Br, OpCodes.Br_S, OpCodes.Brfalse, OpCodes.Brfalse_S, OpCodes.Brtrue, OpCodes.Brtrue_S, // basic branches
             OpCodes.Bge, OpCodes.Bge_S, OpCodes.Bge_Un, OpCodes.Bge_Un_S, OpCodes.Bgt, OpCodes.Bgt_S, OpCodes.Bgt_Un, OpCodes.Bgt_Un_S, // Branch Greater
             OpCodes.Ble, OpCodes.Ble_S, OpCodes.Ble_Un, OpCodes.Ble_Un_S, OpCodes.Blt, OpCodes.Blt_S, OpCodes.Blt_Un, OpCodes.Blt_Un_S, // Branch Less
             OpCodes.Beq, OpCodes.Beq_S, OpCodes.Bne_Un, OpCodes.Bne_Un_S // Branch Equality
         };
 
-		/// <summary>
-		/// Simple check to see if the instruction is a branching instruction (and if so the operand should be a label)
-		/// </summary>
-		/// <param name="instruction">CodeInstruction provided by Harmony.</param>
-		/// <returns>bool, true means it is a branching instruction, false is it's not.</returns>
-		internal static bool isBranch(CodeInstruction instruction)
-		{
-			if (branchOps.Contains(instruction.opcode))
-				return true;
-			return false;
-		}
+        /// <summary>
+        /// Simple check to see if the instruction is a branching instruction (and if so the operand should be a label)
+        /// </summary>
+        /// <param name="instruction">CodeInstruction provided by Harmony.</param>
+        /// <returns>bool, true means it is a branching instruction, false is it's not.</returns>
+        internal static bool isBranch(CodeInstruction instruction)
+        {
+            if (branchOps.Contains(instruction.opcode))
+                return true;
+            return false;
+        }
 
-		/// <summary>
-		/// Utility function to convert a nullable bool (bool?) into a bool (primitive).
-		/// </summary>
-		/// <param name="input">bool? (nullable bool)</param>
-		/// <returns>bool</returns>
-		internal static bool doCast(bool? input)
-		{
-			if (!input.HasValue)
-				return false;
-			return (bool)input;
-		}
+        /// <summary>
+        /// Utility function to convert a nullable bool (bool?) into a bool (primitive).
+        /// </summary>
+        /// <param name="input">bool? (nullable bool)</param>
+        /// <returns>bool</returns>
+        internal static bool doCast(bool? input)
+        {
+            if (!input.HasValue)
+                return false;
+            return (bool)input;
+        }
 
-		/// <summary>
-		/// Return a CodeInstruction object with the correct opcode to fetch a local variable at a specific index.
-		/// </summary>
-		/// <param name="index">int value specifying the local variable index to fetch.</param>
-		/// <param name="info">LocalVariableInfo optional if you've stored the metadata from a load/save instruction previously.</param>
-		/// <returns>CodeInstruction object with the correct opcode to fetch a local variable into the evaluation stack.</returns>
-		internal static CodeInstruction MakeLocalLoadInstruction(int index, LocalVariableInfo info = null)
-		{
-			// argument check...
-			if (index < 0 || index > UInt16.MaxValue)
-				throw new ArgumentException("Index must be greater than 0 and less than " + uint.MaxValue.ToString() + ".");
+        /// <summary>
+        /// Return a CodeInstruction object with the correct opcode to fetch a local variable at a specific index.
+        /// </summary>
+        /// <param name="index">int value specifying the local variable index to fetch.</param>
+        /// <param name="info">LocalVariableInfo optional if you've stored the metadata from a load/save instruction previously.</param>
+        /// <returns>CodeInstruction object with the correct opcode to fetch a local variable into the evaluation stack.</returns>
+        internal static CodeInstruction MakeLocalLoadInstruction(int index, LocalVariableInfo info = null)
+        {
+            // argument check...
+            if (index < 0 || index > UInt16.MaxValue)
+                throw new ArgumentException("Index must be greater than 0 and less than " + uint.MaxValue.ToString() + ".");
 
-			// the first 4 are easy...  (ProfoundDarkness)NOTE: I've only ever observed these with null operands.
-			switch (index)
-			{
-				case 0:
-					return new CodeInstruction(OpCodes.Ldloc_0);
-				case 1:
-					return new CodeInstruction(OpCodes.Ldloc_1);
-				case 2:
-					return new CodeInstruction(OpCodes.Ldloc_2);
-				case 3:
-					return new CodeInstruction(OpCodes.Ldloc_3);
-			}
+            // the first 4 are easy...  (ProfoundDarkness)NOTE: I've only ever observed these with null operands.
+            switch (index)
+            {
+                case 0:
+                    return new CodeInstruction(OpCodes.Ldloc_0);
+                case 1:
+                    return new CodeInstruction(OpCodes.Ldloc_1);
+                case 2:
+                    return new CodeInstruction(OpCodes.Ldloc_2);
+                case 3:
+                    return new CodeInstruction(OpCodes.Ldloc_3);
+            }
 
-			object objIndex;
-			// proper type info for the other items.
-			if (info != null && info.LocalIndex == index)
-				objIndex = info;
-			else
-				objIndex = index;
+            object objIndex;
+            // proper type info for the other items.
+            if (info != null && info.LocalIndex == index)
+                objIndex = info;
+            else
+                objIndex = index;
 
-			if (index > Byte.MaxValue) return new CodeInstruction(OpCodes.Ldloc, objIndex);
-			return new CodeInstruction(OpCodes.Ldloc_S, objIndex);
-		}
+            if (index > Byte.MaxValue) return new CodeInstruction(OpCodes.Ldloc, objIndex);
+            return new CodeInstruction(OpCodes.Ldloc_S, objIndex);
+        }
 
-		/// <summary>
-		/// Return the index of a local variable (based on storage opcode).
-		/// </summary>
-		/// <param name="instruction">CodeInstruction object from Harmony</param>
-		/// <returns>int index of the local variable the instruction refers to. -1 if the opcode wasn't a recognized storage opcode.</returns>
-		internal static int OpcodeStoreIndex(CodeInstruction instruction)
-		{
-			if (instruction.opcode == OpCodes.Stloc_0) return 0;
-			if (instruction.opcode == OpCodes.Stloc_1) return 1;
-			if (instruction.opcode == OpCodes.Stloc_2) return 2;
-			if (instruction.opcode == OpCodes.Stloc_3) return 3;
-			if (instruction.opcode == OpCodes.Stloc_S) // UInt8
-				return (instruction.operand as LocalVariableInfo).LocalIndex;
-			if (instruction.opcode == OpCodes.Stloc) // UInt16
-				return (instruction.operand as LocalVariableInfo).LocalIndex;
-			return -1; // error, unrecognized opcode so can check this if we DIDN'T get an apt opcode.
-		}
+        /// <summary>
+        /// Return the index of a local variable (based on storage opcode).
+        /// </summary>
+        /// <param name="instruction">CodeInstruction object from Harmony</param>
+        /// <returns>int index of the local variable the instruction refers to. -1 if the opcode wasn't a recognized storage opcode.</returns>
+        internal static int OpcodeStoreIndex(CodeInstruction instruction)
+        {
+            if (instruction.opcode == OpCodes.Stloc_0) return 0;
+            if (instruction.opcode == OpCodes.Stloc_1) return 1;
+            if (instruction.opcode == OpCodes.Stloc_2) return 2;
+            if (instruction.opcode == OpCodes.Stloc_3) return 3;
+            if (instruction.opcode == OpCodes.Stloc_S) // UInt8
+                return (instruction.operand as LocalVariableInfo).LocalIndex;
+            if (instruction.opcode == OpCodes.Stloc) // UInt16
+                return (instruction.operand as LocalVariableInfo).LocalIndex;
+            return -1; // error, unrecognized opcode so can check this if we DIDN'T get an apt opcode.
+        }
 
-		/// <summary>
-		/// REturn the index of a local variable (based on load opcode).
-		/// </summary>
-		/// <param name="instruction">CodeInstruction object from Harmony</param>
-		/// <returns>int index of the local variable the instruction refers to.  -1 if the opcode wasn't a recognized load opcode.</returns>
-		internal static int OpcodeLoadIndex(CodeInstruction instruction)
-		{
-			if (instruction.opcode == OpCodes.Ldloc_0) return 0;
-			if (instruction.opcode == OpCodes.Ldloc_1) return 1;
-			if (instruction.opcode == OpCodes.Ldloc_2) return 2;
-			if (instruction.opcode == OpCodes.Ldloc_3) return 3;
-			if (instruction.opcode == OpCodes.Ldloc_S) // UInt8
-				return (instruction.operand as LocalVariableInfo).LocalIndex;
-			if (instruction.opcode == OpCodes.Ldloc) // UInt16
-				return (instruction.operand as LocalVariableInfo).LocalIndex;
-			return -1;
-		}
-		#endregion Utility_Methods
-	}
+        /// <summary>
+        /// REturn the index of a local variable (based on load opcode).
+        /// </summary>
+        /// <param name="instruction">CodeInstruction object from Harmony</param>
+        /// <returns>int index of the local variable the instruction refers to.  -1 if the opcode wasn't a recognized load opcode.</returns>
+        internal static int OpcodeLoadIndex(CodeInstruction instruction)
+        {
+            if (instruction.opcode == OpCodes.Ldloc_0) return 0;
+            if (instruction.opcode == OpCodes.Ldloc_1) return 1;
+            if (instruction.opcode == OpCodes.Ldloc_2) return 2;
+            if (instruction.opcode == OpCodes.Ldloc_3) return 3;
+            if (instruction.opcode == OpCodes.Ldloc_S) // UInt8
+                return (instruction.operand as LocalVariableInfo).LocalIndex;
+            if (instruction.opcode == OpCodes.Ldloc) // UInt16
+                return (instruction.operand as LocalVariableInfo).LocalIndex;
+            return -1;
+        }
+        #endregion Utility_Methods
+    }
 }
